549ecdf4d6d25760881e029cf95b73ccea0a6720
Tony Allard <tony.g.allard@gmail.com>
2016-03-23 18:20:38 +1030

    Added output to timestamp start and end of experiment as well as time taken

--- exp-scripts/runExperiment.sh
+++ exp-scripts/runExperiment.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-ls /mnt/data/problems/*.pddl | parallel ./processProblem.py
+
+date_start=$(date)
+now_start=$(date +%s)
+echo $date_start: Expermient Started
+
+ls /mnt/data/problems/*.pddl | parallel /mnt/data/bin/CEIScripts/exp-scripts/processProblem.py
+
+date_end=$(date)
+now_end=$(date +%s)
+runtime=$( echo "$now_end - $now_start" | bc -l )
+echo $date_end: Experiment Completed
+echo Runtime: $runtime seconds.
