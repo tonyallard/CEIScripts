0ec6f7afba8b634280a2d8ace6b85b11670dd5d8
Tony Allard <tony.g.allard@gmail.com>
2016-04-06 15:52:55 +0930

    Added file for common Analysis methods

---
+++ problem-analysis/AnalysisCommon.py
@@ -0,0 +1,77 @@
+#! /usr/bin/python
+#Author: Tony Allard
+#Date: 06 April 2016
+#Description: Common methods for problem analysis
+
+TIMEOUT_DELIM = "timeout: the monitored command dumped core"
+MEMORY_ERROR_DELIM = "terminate called after throwing an instance of \'std::bad_alloc\'"
+PDDL_FILE_EXT = ".pddl"
+LOG_FILE_EXT = ".pddl.txt"
+SUCCESS_DELIM = ";;;; Solution Found"
+UNSOVLEABLE_DELIM = ";; Problem unsolvable!"
+
+PROBFILE_DELIM = "Prob-"
+SERVER_LOG_DELIM = "explog-"
+
+def hasTimedOut(logFile):
+	for line in logFile:
+		if TIMEOUT_DELIM in line:
+			return True
+	return False
+
+def extractProblemProperties(problemName):
+	cargo = problemName[12:13]
+	tightness = "%s.%s"%(problemName[14:15], problemName[16:problemName.rfind("-")])
+	probNumber = problemName[problemName.rfind("-")+1:-9]
+	probDesc = "%s:%s"%(cargo, tightness)
+	return int(cargo), float(tightness), int(probNumber), probDesc
+
+def isOutOfMemory(logFile):
+	for line in logFile:
+		if MEMORY_ERROR_DELIM in line:
+			return True
+	return False
+
+def isCPUTimeout(logFile):
+	for line in logFile:
+		if TIMEOUT_DELIM in line:
+			return True
+	return False
+
+def isSuccessful(logFile):
+	for line in logFile:
+		if SUCCESS_DELIM in line:
+			return True
+	return False
+
+def isUnsolveable(logFile):
+	for line in logFile:
+		if UNSOVLEABLE_DELIM in line:
+			return True
+	return False
+
+def bufferFile(file):
+	fileBuffer = []
+	for line in file:
+		fileBuffer.append(line)
+	return fileBuffer
+
+def isProblemFile(filename):
+	if PROBFILE_DELIM in filename:
+		return True
+	return False
+
+def isProblemLog(filename):
+	if PROBFILE_DELIM in filename and LOG_FILE_EXT in filename:
+		return True
+	return False
+
+def isServerLog(filename):
+	if SERVER_LOG_DELIM in filename:
+		return True
+	return False
+
+def checkArgs(arg, args):
+	if args.count(arg) > 0:
+		return True
+	return False
