ec08f911eb0436d81d9de52b65278a6c8a118361
Tony Allard <tony.g.allard@gmail.com>
2016-04-06 15:43:33 +0930

    Updated process problem to flush buffer so problem details are printed first and add date to problem log

--- exp-scripts/processProblem.py
+++ exp-scripts/processProblem.py
@@ -3,6 +3,7 @@
 import sys
 import subprocess
 import socket
+import time
 from timeit import Timer
 from shutil import move
 
@@ -31,20 +32,21 @@ except IOError:
 print "Processing %s on %s" %(probFile, socket.gethostname())
 
 #Ouput Logging
+bufsize = 0
 logFileName = probFile + ".txt"
 logFullQual = LOG_FOLDER + logFileName
-log = open(logFullQual, "a")
+log = open(logFullQual, "a", bufsize)
 
 #Setup experiment
 reps=1
 stdout = log
 stderr = log
-parameters = "-h %s %s"%(DOMAIN_FILE, proc_probFile)
+parameters = "-3 %s %s"%(DOMAIN_FILE, proc_probFile)
 external_command = "(cd %s && %s && %s %s %s)"%(COLIN_LOC, MEMLIMIT_CMD, TIMEOUT_CMD, COLIN_EXEC_LOC, parameters)
 call_args = """['%s'], stdout=stdout, stderr=stderr"""%(external_command)
 
 #Run Experiment
-log.write("===Processing %s on %s===\n"%(proc_probFile, socket.gethostname()))
+log.write("===%s: Processing %s on %s===\n"%(time.strftime("%d %m %Y - %H:%M:%S"), proc_probFile, socket.gethostname()))
 log.write("===with Command %s===\n" %external_command)
 t = Timer(stmt = """subprocess.call(%s, shell=True)"""%call_args, setup="""import subprocess; stdout=open(\"%s\", 'a'); stderr=stdout"""%logFullQual)
 timeTaken = t.timeit(reps)
