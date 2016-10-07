5586af807090326a922ba1cd92da8252e05c8e8c
Tony Allard <tony.g.allard@gmail.com>
2016-10-07 17:36:58 +1030

    removed core limitation on runExperiment

--- exp-scripts/runExperiment.sh
+++ exp-scripts/runExperiment.sh
@@ -4,10 +4,10 @@ date_start=$(date)
 now_start=$(date +%s)
 echo $date_start: Expermient Started
 
-ls /mnt/data/problems/*.PDDL | parallel --gnu -j 2 /mnt/data/bin/CEIScripts/exp-scripts/processProblem.py
+ls /mnt/data/problems/*.pddl | parallel --gnu /mnt/data/bin/CEIScripts/exp-scripts/processProblem.py
 
 date_end=$(date)
 now_end=$(date +%s)
 runtime=$( echo "$now_end - $now_start" | bc -l )
 echo $date_end: Experiment Completed
-echo Runtime: $runtime seconds.ch
+echo Runtime: $runtime seconds
