3432ba488fcd467d642608bd2dbd7b223820255e
Tony Allard <tony.g.allard@gmail.com>
2016-11-01 18:00:47 +1030

    Added ability to detect dead ends even from problems that terminated unexpectedly

--- problem-analysis-generic/ExtractDeadEnds.py
+++ problem-analysis-generic/ExtractDeadEnds.py
@@ -11,15 +11,44 @@ import AnalysisCommon
 
 DEAD_END_COUNT_DELIM = "#; Search encountered"
 DEAD_END_COUNT_TERM_DELIM = " dead ends"
+EHC_SEARCH_STARTING = "Initial heuristic = "
+BFS_SEARCH_STARTING = "Resorting to best-first search"
 
 def extractDeadEnds(log):
 	for line in log:
 		if DEAD_END_COUNT_DELIM in line:
-			deadEndCount = re.findall(r'\d+\.\d+', line)
-			if len(deadEndCount) == 0:
-				return 0
+			deadEndCount = re.findall(r'\d+', line)
 			return int(deadEndCount[0])
-	return 0
+	return None
+
+def extractDeadEndsManually(log):
+	#Find all dead ends from EHC
+	deadends = None
+	startLine = 0
+	for line in log:
+		if EHC_SEARCH_STARTING in line:
+			break
+		startLine += 1
+	for line in log[startLine+2:]:
+		deadends = line.count('d')
+		break
+	#Find all dead ends from BFS
+	startLine = 0
+	for line in log:
+		if BFS_SEARCH_STARTING in line:
+			break
+		startLine += 1
+	#Search for deadends until a termination is encountered
+	for line in log[startLine+1:]:
+		termination = AnalysisCommon.getTerminationIndex(line)
+		if termination is -1:
+			termination = len(line)
+		deadends += line[:termination].count('d')
+		#If we found the termination delimeter
+		#then this is the last line to count
+		if termination is not len(line):
+			break
+	return deadends
 
 def main(args):
 	inputPath = args[0]
