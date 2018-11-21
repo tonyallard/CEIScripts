5a227cc8e81d6d2b39d4991e0c388f47e3816f2a
Tony Allard <tony.allard@anu.edu.au>
2018-11-21 15:42:05 +1030

    Updated ExtractFailedProblems.py to work with current log structure (all planners) and differentiate between different failures

--- problem-analysis/ExtractFailedProblems.py
+++ problem-analysis/ExtractFailedProblems.py
@@ -5,62 +5,151 @@
 
 import os
 import sys
+import gzip
+import re
 import AnalysisCommon
+import ExtractSuccess
 
-SERVER_ATTEMPT_DELIM = "processing "
-LOG_FILE_EXT = ".txt"
+timeout_check = re.compile(AnalysisCommon.TIMEOUT_DELIM)
+mem_check = re.compile("%s|%s"%(AnalysisCommon.MEMORY_ERROR_DELIM, \
+		AnalysisCommon.LPG_TIMEOUT_DELIM))
+unsolvable_check = re.compile(AnalysisCommon.SEARCH_FAILURE_DELIM)
+invalidPlan_check = re.compile(AnalysisCommon.INVALID_PLAN_DELIM)
+
+def isMemoryFail(log):
+	for line in log:
+		#check problem for memory crash
+		if mem_check.search(line) is not None:
+			return True
+	return False
+
+def isTimeOutFail(log):
+	for line in log:
+		#check problem for timeout
+		if timeout_check.search(line) is not None:
+			return True
+	return False
+
+def isUnsolvable(log):
+	for line in log:
+		#check problem for timeout
+		if unsolvable_check.search(line) is not None:
+			return True
+	return False
+
+def isInvalidPlan(log):
+	for line in log:
+		#check problem for timeout
+		if invalidPlan_check.search(line) is not None:
+			return True
+	return False
+
+def getLogStructure(rootDir):
+	logStructure = {}
+	for planner in os.listdir(rootDir):
+		plannerDir = os.path.join(rootDir, planner)
+		
+		if (not os.path.isdir(plannerDir)):
+			continue
+		
+		logStructure[planner] = {}
+
+		for problem in os.listdir(plannerDir):
+			#Driverlog shift is too different a format
+			if problem == "driverlogshift":
+				continue
+			logDir = os.path.join(plannerDir, problem, AnalysisCommon.OUTPUT_DIR)
+			logStructure[planner][problem] = logDir
+	return logStructure
 
 def main(args):
-	path = "/home/tony/Desktop/Colin-TRH-logs/"
-	server = "tga-cei02"
+	inputPath = args[1]
+	verbose = False
+	if len(args) > 2:
+		verbose = True
 
-	serverProblems = []
+	logStructure = getLogStructure(inputPath)
 
-	memFails = []
-	timeoutFails = []
-	NoFails = []
+	for planner in logStructure:
+		print planner
+		for problemDomain in logStructure[planner]:
+			logPath = logStructure[planner][problemDomain]
 
-	#Find server log file
-	for filename in os.listdir(path):
-		if server in filename:
-			serverLog = open(path+filename, 'r')
+			memFailProbe = []
+			timeoutFailProbs = []
+			unsolvableProbs = []
+			invalidPlanProbs = []
+			otherFailProbs = []
+			success = 0
+			failure = 0
+			memFails = 0
+			timeoutFails = 0
+			unsolvableFails = 0
+			invalidPlanFails = 0
+			otherFails = 0
 
-			#extract problems server attempted
-			for line in serverLog:
-				if SERVER_ATTEMPT_DELIM.lower() in line.lower() and server.lower() in line.lower():
-					preamble = line.lower().index(SERVER_ATTEMPT_DELIM.lower()) + len(SERVER_ATTEMPT_DELIM.lower())
-					post = line.lower().index(AnalysisCommon.PDDL_FILE_EXT.lower())+len(AnalysisCommon.PDDL_FILE_EXT.lower())
-					problemName = line[preamble:post]
-					serverProblems.append(problemName)
+			print "\t%s"%problemDomain
+
+			for filename in os.listdir(logPath):
+				fullQualified = os.path.join(logPath, filename)
+				with gzip.open(fullQualified, 'rb') as f:
+					try:
+						buffer = AnalysisCommon.bufferFile(f)
+					except IOError:
+						continue
+				if not AnalysisCommon.isProblemLog(filename, buffer):
+					continue
+				if ((planner == "lpg-td" and \
+						ExtractSuccess.extractLPGTDSuccess(buffer, fullQualified)) or \
+						(ExtractSuccess.extractValidatorSuccess(buffer, fullQualified))):
+						success += 1
+				else:
+					failure += 1
+					if isMemoryFail(buffer):
+						memFailProbe.append(filename)
+						memFails += 1
+					elif isTimeOutFail(buffer):
+						timeoutFailProbs.append(filename)
+						timeoutFails += 1
+					elif isUnsolvable(buffer):
+						unsolvableProbs.append(filename)
+						unsolvableFails += 1
+					elif isInvalidPlan(buffer):
+						invalidPlanProbs.append(filename)
+						invalidPlanFails += 1
+					else:
+						otherFailProbs.append(filename)
+						otherFails += 1
 			
-	for problem in serverProblems:
-		log = open(path+problem+LOG_FILE_EXT, 'r')
-		seemsOK = True
-		for line in log:
-			#check problem for memory crash
-			if AnalysisCommon.MEMORY_ERROR_DELIM in line:
-				memFails.append(problem)
-				seemsOk = False
-			#check problem for timeout
-			if AnalysisCommon.TIMEOUT_DELIM in line:
-				timeoutFails.append(problem)
-				seemsOK = False
-		if seemsOK :
-			NoFails.append(problem)
+			print "\t\tSuccess: %i"%success
+			print "\t\tFailure: %i (%i memory, %i timeout, %i unsolvable, %i invalid plan, %i other)"\
+				%(failure, memFails, timeoutFails, unsolvableFails, invalidPlanFails, otherFails)
+			if verbose:
+				#Print problems
+				if memFails > 0:
+					print "\t\t\tProblems that ran out of memory:"
+					for prob in memFailProbe:
+						print "\t\t\t\t%s"%prob
 
-	#Print problems
-	print "Problems that ran out of memory"
-	for prob in memFails:
-		print prob
+				if timeoutFails > 0:
+					print "\n\t\t\tProblems that exceeded CPU time:"
+					for prob in timeoutFailProbs:
+						print "\t\t\t\t%s"%prob
 
-	print "\nProblems that exceeded CPU time"
-	for prob in timeoutFails:
-		print prob
+				if unsolvableFails > 0:
+					print "\n\t\t\tProblems that were unsolvable:"
+					for prob in unsolvableProbs:
+						print "\t\t\t\t%s"%prob
 
-	print "\nProblems that seem OK"
-	for prob in NoFails:
-		print prob
+				if invalidPlanFails > 0:
+					print "\n\t\t\tProblems where the plan found was invlaid:"
+					for prob in invalidPlanProbs:
+						print "\t\t\t\t%s"%prob
 
+				if otherFails > 0:
+					print "\n\t\t\tProblems failed for other reasons:"
+					for prob in otherFailProbs:
+						print "\t\t\t\t%s"%prob
 
 #Run Main Function
 if __name__ == "__main__":
