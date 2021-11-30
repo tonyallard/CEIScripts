7e74c6ed9d5d80fd7a41f5e0424db1d1b613e9ae
Tony Allard <tony.allard@anu.edu.au>
2021-11-30 21:48:57 +1100

    Fixed errors in instal file

--- exp-scripts/install.sh
+++ exp-scripts/install.sh
@@ -2,8 +2,8 @@
 
 EXP_ROOT_DIR=$(pwd)
 
-if [[ -d "$2" ]]; then
-   EXP_ROOT_DIR=$(realpath "$2")
+if [[ -d "$1" ]]; then
+   EXP_ROOT_DIR=$(realpath "$1")
    echo "Path set to $EXP_ROOT_DIR"
    
 else
@@ -12,11 +12,11 @@ else
 fi
 
 echo "Creating pyton virtual environment"
-python3 -m venv exp-scripts
+python3 -m venv "$EXP_ROOT_DIR"/exp-scripts
 echo "Activating environment"
-source ./exp-scripts/bin/activate
+source "$EXP_ROOT_DIR"/exp-scripts/bin/activate
 echo "Installing required python3 packages"
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r "$EXP_ROOT_DIR"/exp-scripts/requirements.txt
 echo "Deactivating environment"
 deactivate
 echo "Complete."
